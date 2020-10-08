9b37ee0bda006e24639fd1e1f6786f0e2a991c0e
liyang <xxbarca@163.com>
2020-10-08 13:41:02 +0800

    Swiper

---
+++ models/Banner.js
@@ -0,0 +1,23 @@
+import instance from "../utils/http"
+
+class Banner {
+	
+	static locationB = 'b-1'
+	static locationG = 'b-2'
+	
+	static async getHomeLocationB() {
+		return await instance.request({
+			method: 'GET',
+			url: `/api/v1/banner/name/${Banner.locationB}`,
+		})
+	}
+	
+	static async getHomeLocationG() {
+		return await instance.request({
+			method: "GET",
+			url: `/api/v1/banner/name/${Banner.locationG}`,
+		})
+	}
+}
+
+export {Banner}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,17 @@
 <template>
 	<img class="top-theme" :src="themeA.entrance_img" />
+	<van-swipe :autoplay="1000" indicator-color="white">
+		<van-swipe-item v-for="(item, index) in bannerB.items" :key="index">
+			<img class="banner_image" :src="item.img" />
+		</van-swipe-item>
+	</van-swipe>
 	<SpuScroll :theme="themeE" :spu-list="themeESpu" />
 </template>
 
 <script>
 	import {onMounted, reactive, toRefs} from 'vue'
 	import {Theme} from "../../../models/Theme"
+	import {Banner} from '../../../models/Banner'
 	import SpuScroll from '../../components/spu-scroll'
 	export default {
 		name: "home",
@@ -16,7 +22,8 @@
 			let state = reactive({
 				themeA: {},
 				themeE: {},
-				themeESpu: []
+				themeESpu: [],
+				bannerB: [],
 			})
 			onMounted(() => {
 				methods.initAllData()
@@ -29,6 +36,8 @@
 
 					state.themeA = theme.getHomeLocationA()
 					state.themeE = theme.getHomeLocationE()
+					state.bannerB = await Banner.getHomeLocationB()
+					console.log(state.bannerB)
 
 					if (state.themeE.online) {
 						const data = await Theme.getHomeLocationESpu()
